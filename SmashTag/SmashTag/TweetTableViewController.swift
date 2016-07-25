79414082dbc8a85ea17e50b3ef01e0c4d994cdf6
Vladimir Romanov <vladromweb@gmail.com>
2016-07-25 10:44:10 -0700

    SmashTag - Initial Commit &clean UITableViewController
    SmashTag - Initial Commit &clean UITableViewController

---
+++ SmashTag/SmashTag/TweetTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TweetTableViewController.swift
+//  SmashTag
+//
+//  Created by Volodymyr Romanov on 7/25/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

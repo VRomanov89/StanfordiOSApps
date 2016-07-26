637fe0bb871f1a9ffb72c64edb56454c0e735000
Vladimir Romanov <vladromweb@gmail.com>
2016-07-26 12:45:40 -0700

    SmashTag - Core Data Initialization
    SmashTag - Core Data Initialization

---
+++ SmashTag/SmashTag/TweetersTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TweetersTableViewController.swift
+//  SmashTag
+//
+//  Created by Volodymyr Romanov on 7/26/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import UIKit
+
+class TweetersTableViewController: UITableViewController {
+
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+
+}

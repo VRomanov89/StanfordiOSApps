4c2a13044340e188b10eb111519da87d514a822b
Vladimir Romanov <vladromweb@gmail.com>
2016-07-27 09:50:02 -0700

    DropIt - Initial creation of view and controller
    DropIt - Initial creation of view and controller

---
+++ DropItApp/DropItApp/DropItView.swift
@@ -0,0 +1,13 @@
+//
+//  DropItView.swift
+//  DropItApp
+//
+//  Created by Volodymyr Romanov on 7/27/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import UIKit
+
+class DropItView: UIView {
+    
+}
---
+++ DropItApp/DropItApp/DropItViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DropItViewController.swift
+//  DropItApp
+//
+//  Created by Volodymyr Romanov on 7/27/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import UIKit
+
+class DropItViewController: UIViewController {
+    @IBOutlet weak var gameView: DropItView!
+    
+}
